019226a22150523163db8749951678822cab15cb
AruzhanG <157003706+AruzhanG@users.noreply.github.com>
2024-01-24 20:29:02 +0600

    multi words

---
+++ lab1/variables/ex8.py
@@ -0,0 +1,3 @@
+myVariableName = "John" # "Camel case" -  each word, except first, starts with a capital letter
+MyVariableName = "John" # "Pascal case" - each word starts with a capital letter
+my_variable_name = "John" # "Snake case" - each word is separated by an unserscore character
